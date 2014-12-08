1d6b0ef53f75e77b6faa1326152ed66a012b1422
Tomas Levin <tomas.levin@cacanny.net>
2014-12-08 21:43:26 +0100

    Prototype of tables in quasi JSON format
    A testing example for the quasi JSON format, this needs to be built
    later from the excel spreadsheet

---
+++ RoadEmissions/tier3/PC/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  5 20:58:19 2014
+
+@author: tlev
+"""
+
+"""
+Matematical emission fuctions without parmeters, the function reference
+nubers are accoring to the EEA 2013 Guidebook 
+"""
+
+PC_CO = {
+    'Euro 1':
+    { 'function': 21,
+      'a' : 1, 
+      'b' : 2,
+      'c' : 3,
+      'd' : 4,
+      'e' : 5,
+      'f' : 6
+    }
+
+}
+    
+    
+    
